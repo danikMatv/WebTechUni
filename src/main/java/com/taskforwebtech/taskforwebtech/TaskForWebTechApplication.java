d464fc10001fef8c588ba15342f604557aa88e1a
daniilMatviichuk <168640672+daniilMatviichuk@users.noreply.github.com>
2025-03-09 20:14:08 +0200

    c

---
+++ src/main/java/com/taskforwebtech/taskforwebtech/TaskForWebTechApplication.java
@@ -0,0 +1,13 @@
+package com.taskforwebtech.taskforwebtech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskForWebTechApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaskForWebTechApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/taskforwebtech/taskforwebtech/TaskForWebTechApplicationTests.java
@@ -0,0 +1,13 @@
+package com.taskforwebtech.taskforwebtech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskForWebTechApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
